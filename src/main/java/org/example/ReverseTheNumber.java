989b12f803eb2767bbd5a3f2aaaa60e8aec19195
karpit <coronanegative@hotmail.com>
2023-10-12 11:52:37 -0700

    Reverse was good

---
+++ src/main/java/org/example/ReverseTheNumber.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ReverseTheNumber {
+    public static void main(String[] args) {
+
+
+        // Reverse the number
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number you want to reverse ");
+        int n = in.nextInt();
+
+        int ans = 0;
+
+        while(n>0){
+
+            int mod = n % 10;
+            n = n / 10;
+
+            ans = ans * 10 + mod;
+        }
+        System.out.println("The reverse of the nunber will be " + ans);
+
+    }
+}
